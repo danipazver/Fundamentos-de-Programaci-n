a99f4a1a60058eede2f4b398bf779a1a2ed9d1bf
danipazver <69821574+danipazver@users.noreply.github.com>
2020-08-17 17:18:07 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+//Daniel Paz//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hola mundo";
+
+    return 0;
+}
