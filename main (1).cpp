745eedc81397debe0a7ebf243a09c04c455f2194
danipazver <69821574+danipazver@users.noreply.github.com>
2020-08-17 17:50:22 -0400

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+//Daniel Paz//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a, b, c ;
+   
+   cout<<"el primer digito es:";
+   cin >> a;
+   cout<<"el segundo digito es:";
+   cin >> b;
+   c = a+ b;
+   cout << "La suma es: " << c;
+
+    return 0;
+}
